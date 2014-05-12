4c3c9a5f8b7c641b49da61cc7c0335c399979e11
yilinx <yilinx@gmail.com>
2014-05-12 15:58:42 +0800

    first commit complete.R assignment

---
+++ complete.R
@@ -0,0 +1,32 @@
+complete <- function(directory, id = 1:332) {
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+        
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
+        
+        ## Return a data frame of the form:
+        ## id nobs
+        ## 1  117
+        ## 2  1041
+        ## ...
+        ## where 'id' is the monitor ID number and 'nobs' is the
+        ## number of complete cases
+        
+        directory <- "E:\\Coursera Data Scientist\\R Programming\\Programming Assignment 1\\specdata\\"
+        CCTotal <- data.frame()
+         
+        
+        for (i in id) {
+                inew <- formatC(i, width=3, flag="0")
+                FileLocation <- paste(directory, inew, ".csv", sep="")
+                x <- read.csv(FileLocation)
+                CCList <- complete.cases (x["sulfate"],x["nitrate"])
+                CCVal <- c(i, sum(CCList))
+                CCTotal <- rbind(CCTotal, CCVal)
+        }
+        
+        names(CCTotal) <- c("id","nobs")
+        CCTotal
+        
+}
